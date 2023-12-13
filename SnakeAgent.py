4c747b98d63fb6fc971218b5e161111d40652d64
Oscar Tegnér Mohringe <58402563+oscar-tm@users.noreply.github.com>
2023-12-13 23:32:56 +0100

    Outline of DQNAgent class added.

---
+++ SnakeAgent.py
@@ -0,0 +1,18 @@
+class DQNAgent:
+    def __init__(self) -> None:
+        pass
+
+    def forward():
+        pass
+
+    def move():
+        pass
+
+    def addToMem():
+        pass
+
+    def sampleMem():
+        pass
+
+    def optimizeModel():
+        pass
