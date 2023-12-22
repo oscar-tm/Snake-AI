e4346e6e73ae7a55c6f697172436640550519f32
Oscar Tegnér Mohringe <58402563+oscar-tm@users.noreply.github.com>
2023-12-22 22:25:28 +0100

    2-D conv now works as intended.

--- SnakeDQNAgent.py
+++ SnakeDQNAgent.py
@@ -6,36 +6,37 @@ import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class DQNAgent:
-    def __init__(self, memSize = 10000) -> None:
-        self.cache = deque(maxlen = memSize)
+    def __init__(self, memSize=10000) -> None:
+        self.cache = deque(maxlen=memSize)
         self.onlineNet = nn.Sequential(
-            nn.LazyConv1d(1, 3, device = device),
+            nn.LazyConv2d(1, 3, device=device),
             nn.LeakyReLU(),
-            nn.LazyConv1d(1, 3, device = device),
+            nn.LazyConv2d(1, 3, device=device),
             nn.LeakyReLU(),
             nn.Flatten(1),
-            nn.LazyLinear(64, device = device),
+            nn.LazyLinear(64, device=device),
             nn.LeakyReLU(),
-            nn.LazyLinear(16, device = device),
+            nn.LazyLinear(16, device=device),
             nn.LeakyReLU(),
-            nn.LazyLinear(4, device = device)
+            nn.LazyLinear(4, device=device),
         ).to(device)
         self.targetNet = nn.Sequential(
-            nn.LazyConv1d(1, 3, device = device),
+            nn.LazyConv2d(1, 3, device=device),
             nn.LeakyReLU(),
-            nn.LazyConv1d(1, 3, device = device),
+            nn.LazyConv2d(1, 3, device=device),
             nn.LeakyReLU(),
             nn.Flatten(1),
-            nn.LazyLinear(64, device = device),
+            nn.LazyLinear(64, device=device),
             nn.LeakyReLU(),
-            nn.LazyLinear(16, device = device),
+            nn.LazyLinear(16, device=device),
             nn.LeakyReLU(),
-            nn.LazyLinear(4, device = device)
-        )
+            nn.LazyLinear(4, device=device),
+        ).to(device)
         self.targetNet.load_state_dict(self.onlineNet.state_dict())
 
-    def forward(self, x, online = True):
+    def forward(self, x, online=True):
         """
         Sends an input x to the correct net. And return the net output
         """
@@ -53,21 +54,26 @@ class DQNAgent:
                 return torch.argmax(self.forward(self.convertInput(x)))
 
         else:
-            return random.randint(0,3)
+            return random.randint(0, 3)
 
-    def convertInput(self, x, gameSize = 13):
-        """"
-        Converts the int[,] from the game to a tensor.
+    def convertInput(self, x, gameSize=13):
+        """ "
+        Converts the int[,] from the game to a 3-D tensor.
         Inputs: X the int[,] array to be converted, gameSize is the boardsize of the game.
         Returns: The converted tensor
         """
         gM = []
-        for i in range(gameSize+2):
+        # Can be made faster by using only built-in torch functions. Will do later
+        for i in range(gameSize + 2):
             tmp = []
-            for j in range(gameSize+2):
+            for j in range(gameSize + 2):
                 tmp.append(x[i, j])
             gM.append(tmp)
-        return torch.div(torch.stack([torch.Tensor(a) for a in zip(*gM)]).to(device), 7) #Need to transpose the game matrix here for now, will be fixed later. Not a high priority
+        return torch.div(
+            torch.stack([torch.Tensor(a) for a in zip(*gM)]).to(device), 7
+        ).reshape(
+            1, gameSize + 2, gameSize + 2
+        )  # Need to transpose the game matrix here for now, will be fixed later. Not a high priority
 
     def optimizeModel(self):
         pass
